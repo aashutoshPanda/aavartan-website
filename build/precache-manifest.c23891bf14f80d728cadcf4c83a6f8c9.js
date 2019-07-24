e3001df6546351b5e0b3c2db88f004f02fa64271
Ashutosh Panda <iamashutoshpanda@gmail.com>
2019-07-25 01:39:22 +0530

    Build added

---
+++ build/precache-manifest.c23891bf14f80d728cadcf4c83a6f8c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8efc0ba504b1739b417f6a3ca8171ef",
+    "url": "/aavartan-website/index.html"
+  },
+  {
+    "revision": "74b7802087d625a5488c",
+    "url": "/aavartan-website/static/css/main.8efefec4.chunk.css"
+  },
+  {
+    "revision": "732f894b4fd5adcf4f0b",
+    "url": "/aavartan-website/static/js/2.4bb922b5.chunk.js"
+  },
+  {
+    "revision": "74b7802087d625a5488c",
+    "url": "/aavartan-website/static/js/main.569c2e9e.chunk.js"
+  },
+  {
+    "revision": "fb2454562bb92b36d9f3",
+    "url": "/aavartan-website/static/js/runtime~main.7217ea4f.js"
+  }
+]);
